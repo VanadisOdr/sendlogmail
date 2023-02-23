7ab410ef9d9df8bd67b223e0632de5316262f09a
VanadisOdr <oleggnedov@mail.ru>
2023-02-23 18:22:38 +0300

    making changes to dev

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello world!')
+from loguru import logger
+
+logger.add('debug.log', format='{time} {level} {message}',
+level='DEBUG', rotation='10:00', compression='zip')
+
+def MySumLog(num1, num2):
+    return num1/num2
+
+try:
+    MySumLog(1,0)
+except Exception as err:
+    logger.error(err)
